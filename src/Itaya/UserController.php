505c9e96d734c5f563f96f9ca4c061b9dfd90230
Tom Ploskina Jr <tmpjr@gmail.com>
2013-05-06 13:05:18 -0700

    setup Sentry boilerplate

--- src/Itaya/UserController.php
+++ src/Itaya/UserController.php
@@ -2,22 +2,30 @@
 
 namespace Itaya;
 
+use Silex\Application;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use InvalidArgumentException;
 
 class UserController
 {
 	private $app;
 
-	public function __construct($app)
+	public function __construct(Application $app)
 	{
 		$this->app = $app;
 	}
 
 	public function indexAction()
 	{
+		$user = $this->app['sentry']->getUserProvider()->findById(1);
 		$sql = "SELECT * FROM genes";
 		$gene = $this->app['db']->fetchAssoc($sql);
 		$this->app['monolog']->addDebug(print_r($gene,true));
-		return new JsonResponse($gene);
+		return new JsonResponse($user);
+	}
+
+	public function createAction()
+	{
+
 	}
 }
--- web/app.php
+++ web/app.php
@@ -10,22 +10,30 @@ require __DIR__ . '/../resources/config/dev.php';
 $app->register(new Silex\Provider\DoctrineServiceProvider(), $app['db.options']);
 
 // Configure Sentry
-class_alias('Cartalyst\Sentry\Facades\Native\Sentry', 'Sentry');
-Sentry::setupDatabaseResolver(new PDO(
-	$app['db.dsn'], 
-	$app['db.options']['user'], 
-	$app['db.options']['password']
-));
+$app['sentry'] = $app->share(function() use ($app) {
+    $hasher = new Cartalyst\Sentry\Hashing\NativeHasher;
+    $userProvider = new Cartalyst\Sentry\Users\Eloquent\Provider($hasher);
+    $groupProvider = new Cartalyst\Sentry\Groups\Eloquent\Provider;
+    $throttleProvider = new Cartalyst\Sentry\Throttling\Eloquent\Provider($userProvider);
+    $session = new Cartalyst\Sentry\Sessions\NativeSession;
+    $cookie = new Cartalyst\Sentry\Cookies\NativeCookie(array());
 
-$user = Sentry::getUserProvider()->create(array(
-    'email'    => 'tploskinajr@gmail.com',
-    'password' => 'test123',
-    'permissions' => array(
-        'test'  => 1,
-        'other' => -1,
-        'admin' => 1
-    )
-));
+    $sentry = new Cartalyst\Sentry\Sentry(
+        $userProvider,
+        $groupProvider,
+        $throttleProvider,
+        $session,
+        $cookie
+    );
+
+    Cartalyst\Sentry\Facades\Native\Sentry::setupDatabaseResolver(new PDO(
+        $app['db.dsn'], 
+        $app['db.options']['user'], 
+        $app['db.options']['password']
+    ));
+
+    return $sentry;
+});
 
 // Register logging
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
